6417313b1d5b8e461edc7774008ebee63bb2c944
kachena <kachena.masimba@gmail.com>
2023-07-25 01:43:50 +0200

    Initial commit

---
+++ src/main/java/za/co/kachena/NumberRangeSummarizerImpl.java
@@ -0,0 +1,39 @@
+package za.co.kachena;
+
+import java.util.*;
+
+public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
+    @Override
+    public Collection<Integer> collect(String input) {
+        List<String> numbersListString = Arrays.asList(input.split(","));
+        List<Integer> numbersListInteger = numbersListString.stream().map(s -> Integer.parseInt(s.trim())).toList();
+        return new LinkedList<>(numbersListInteger);
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        Queue <Integer> numbersQueue = (Queue<Integer>) input;
+        StringBuilder finalNumbersList = new StringBuilder();
+        while (!numbersQueue.isEmpty()){
+            Integer firstNumber = numbersQueue.poll();
+            Integer secondNumber = numbersQueue.peek();
+
+            boolean isBeginningOfRange = finalNumbersList.isEmpty()||finalNumbersList.toString().endsWith(",");
+            boolean isSequential = firstNumber.equals(secondNumber-1);
+
+            if(isSequential && isBeginningOfRange){
+                finalNumbersList.append(firstNumber).append("-");
+            }else if(isSequential){
+                if(numbersQueue.size()!=1){continue;}
+            }else if(numbersQueue.size()!=1){
+                finalNumbersList.append(firstNumber).append(",");
+            }
+
+            if(numbersQueue.size()==1){
+                finalNumbersList.append(secondNumber);
+                break;
+            }
+        }
+        return finalNumbersList.toString();
+    }
+}
---
+++ src/test/java/za/co/kachena/NumberRangeSummarizerTest.java
@@ -0,0 +1,44 @@
+package za.co.kachena;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+
+public class NumberRangeSummarizerTest {
+    NumberRangeSummarizer numberRangeSummarizer;
+    String input1 = "1,4,5,4,6,5,7,6,5,7,6,7";
+    String input2 = "100,4678,5,4,605605,605606,605607,0,5,7,6,7";
+    String input3 = "1,2,3,4,5,6,7,8,9,10,11,12";
+    String input4 = "12,11,10,9,8,7,77,76,75,74,6,7";
+    @Before
+    public void setup() {
+        numberRangeSummarizer = new NumberRangeSummarizerImpl();
+    }
+
+    @Test
+    public void test_Collect(){
+        Assert.assertNotNull(input1);
+        Assert.assertTrue(input1.matches("(\\d+,)+\\d+"));
+        Assert.assertEquals(List.of(1,4,5,4,6,5,7,6,5,7,6,7), numberRangeSummarizer.collect(input1));
+        Assert.assertNotNull(input2);
+        Assert.assertTrue(input2.matches("(\\d+,)+\\d+"));
+        Assert.assertEquals(List.of(100,4678,5,4,605605,605606,605607,0,5,7,6,7), numberRangeSummarizer.collect(input2));
+        Assert.assertNotNull(input3);
+        Assert.assertTrue(input3.matches("(\\d+,)+\\d+"));
+        Assert.assertEquals(List.of(1,2,3,4,5,6,7,8,9,10,11,12), numberRangeSummarizer.collect(input3));
+        Assert.assertNotNull(input4);
+        Assert.assertTrue(input4.matches("(\\d+,)+\\d+"));
+        Assert.assertEquals(List.of(12,11,10,9,8,7,77,76,75,74,6,7), numberRangeSummarizer.collect(input4));
+    }
+
+    @Test
+    public void test_SummarizeCollection(){
+        Assert.assertEquals("1,4-5,4,6,5,7,6,5,7,6-7", numberRangeSummarizer.summarizeCollection(numberRangeSummarizer.collect(input1)));
+        Assert.assertEquals("100,4678,5,4,605605-605607,0,5,7,6-7", numberRangeSummarizer.summarizeCollection(numberRangeSummarizer.collect(input2)));
+        Assert.assertEquals("1-12", numberRangeSummarizer.summarizeCollection(numberRangeSummarizer.collect(input3)));
+        Assert.assertEquals("12,11,10,9,8,7,77,76,75,74,6-7", numberRangeSummarizer.summarizeCollection(numberRangeSummarizer.collect(input4)));
+    }
+
+
+
+}
